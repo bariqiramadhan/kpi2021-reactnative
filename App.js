5a428392fc91d551e676fefbd0d363713b2574f4
Bariqi Ramadhan <84390530+bariqiramadhan@users.noreply.github.com>
2022-05-09 20:37:14 +0700

    kpi 2021

---
+++ App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+import {Text,View} from 'react-native'
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  };
+  }
+  render() {
+    return (
+      <View>
+        <Text>
+          KPI React Native
+        </Text>
+      </View>
+    );
+  }
+}
+
+export default App;
